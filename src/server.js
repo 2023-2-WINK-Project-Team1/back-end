188763e2c58578695a5dfe431ed82696eddff3f5
p0tat019 <sang0419a@gmail.com>
2023-12-31 16:08:23 +0900

    Rename Student to User

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import testRouter from "./routers/testRouter";
+import userRouter from "./routers/UserRouter";
 import bodyParser from "body-parser";
 
 const app = express();
@@ -12,6 +13,9 @@ app.get("/", (req, res) => {
   console.log("home");
   res.send("home");
 });
+
+// BASEURL/User로 들어온 요청은 UserRouter로 전달한다.
+app.use("/User", userRouter);
 
 // BASEURL/test로 들어온 요청은 testRouter로 전달한다.
 app.use("/test", testRouter);
