7d2c0e5c13fe1ed747213daca6433a786d63b453
p0tat019 <sang0419a@gmail.com>
2023-12-31 16:04:28 +0900

    Rename studentRouter.js to UserRouter.js , RESTful 적용 코맨트에 따라  Student -> User로 변경 User_Id를 가독성을 위해 UserId로 변경 RESTful API 규칙 적용 /notification/:userId -> /:userId/notification

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -3,20 +3,18 @@ import {
     getUserProfile,
     getUserNotifications,
     updateUserNotificationSetting,
-} from '../controllers/studentController';
+} from '../controllers/UserController';
 
 // BASEURL/student로 들어오는 요청을 전담한다.
-const studentRouter = express.Router();
+const userRouter = express.Router();
 
 // 따라서, 이 파일에서 루트 ( = "/") 가 의미하는 경로는 BASEURL/student가 된다.
 
 // 프로필 조회
-studentRouter.get("/:user_Id", getUserProfile);
-
+userRouter.get("/:userId", getUserProfile);
 // 알림 조회
-studentRouter.get("/notification", getUserNotifications);
-
+userRouter.get("/:userId/notification", getUserNotifications);
 // 알림 설정 업데이트
-studentRouter.post("/notification/:user_Id", updateUserNotificationSetting);
+userRouter.post("/:userId/notification", updateUserNotificationSetting);
 
-export default studentRouter;
+export default userRouter;
