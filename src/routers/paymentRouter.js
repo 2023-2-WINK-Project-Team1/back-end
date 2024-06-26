10137bb69d896b68a536162474bba367da7a70a1
Hyeonseung-lee <dlgustmd3590@gmail.com>
2024-06-26 16:29:01 +0900

    Fix:관리자 인증 미들웨어 위치수정, totalDataCnt 수정

--- src/routers/paymentRouter.js
+++ src/routers/paymentRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 import {
-    getPayment,
-    createPayment,
-    getPaymentCsv
+  getPayment,
+  createPayment,
+  getPaymentCsv,
 } from "../controllers/paymentControllers";
 import { authManager } from "../controllers/userController";
 
@@ -11,9 +11,9 @@ const paymentRouter = express.Router();
 
 // 따라서, 이 파일에서 루트 ( = "/") 가 의미하는 경로는 BASEURL/payment가 된다.
 
-paymentRouter.get("/", getPayment,authManager);
-paymentRouter.post("/", createPayment,authManager);
+paymentRouter.get("/", authManager, getPayment);
+paymentRouter.post("/", authManager, createPayment);
 //paymentRouter.delete("/:user_id", deletePayment,authManager); 위험요소로 제거합니다.
-paymentRouter.get("/csv",getPaymentCsv,authManager);
+paymentRouter.get("/csv", authManager, getPaymentCsv);
 
 export default paymentRouter;
